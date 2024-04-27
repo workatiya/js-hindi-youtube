b17137cea3afd26144d6e34d97df94ae64244306
workatiya <127822990+workatiya@users.noreply.github.com>
2024-04-27 10:48:31 +0000

    add 05_strings file update

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,40 @@
+const name = "hitesh"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value"); //(this is old concat) 
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); //( this is new concat )
+
+const gameName = new String('hitesh-hc-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "   hitesh    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/trim  (link for learning TRIM)
+
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'));
+
+// ** when ever you free most imp practice for string **
