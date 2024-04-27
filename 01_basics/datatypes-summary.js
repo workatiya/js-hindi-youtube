57fccf21ef3256eebab35e48b81b640c7ffafce5
workatiya <127822990+workatiya@users.noreply.github.com>
2024-04-27 08:46:17 +0000

    datatypes summary stack and heap add

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -35,3 +35,29 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// ===========================================================================================
+
+
+// stack (primitive), heap  (Non primitive)
+
+// stack
+let myYoutubename = "hiteshchoudharydotcom"
+
+let anothername = myYoutubename 
+    anothername = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+// heap
+let userOne = {
+email: "user@google.com",
+upi: "user@ybl "    
+}
+
+let userTwo = userOne
+userTwo.email= "hitesh@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
